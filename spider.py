ee0d00570983f639bb9f7f37af89e0c29b81e938
root <root@localhost.localdomain>
2019-10-15 22:54:46 -0400

    first commit

--- spider.py
+++ spider.py
@@ -12,9 +12,17 @@ def extract_links(url):
     return re.findall('(?:href=")(.*?)"', response.content)
 
 
-href_links = extract_links(target_url)
-for link in href_links:
-    link = urlparse.urljoin(target_url, link)
-    if target_url in link and link not in target_links:
-        target_links.append(link)
-        print(link)
+def crawl(url):
+    href_links = extract_links(target_url)
+    for link in href_links:
+        link = urlparse.urljoin(url, link)
+        if "#" in link:
+            link = link.split("#")[0]
+
+        if target_url in link and link not in target_links:
+            target_links.append(link)
+            print(link)
+            crawl(link)
+
+
+crawl(target_url)
